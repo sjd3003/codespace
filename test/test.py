39e35651e2b0f14dc403e26141fd9a73b7c38d41
sjd3003 <143242493+sjd3003@users.noreply.github.com>
2023-09-07 18:17:49 +0000

    Thu, Sep 7, 2023, 6:17 PM +00:00

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-url = 'file:///C:/Users/sh/Documents/VeriFone/SRNTemp/radBA089.tmp_PLU.html'
+url = 'https://Users/sh/Documents/VeriFone/SRNTemp/radBA089.tmp_PLU.html'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 print (soup)
