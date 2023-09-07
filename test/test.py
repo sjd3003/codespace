1857b5020ea4697ac0b7b4c0aaeff6278a0c1dd9
sjd3003 <143242493+sjd3003@users.noreply.github.com>
2023-09-07 18:45:15 +0000

    Thu, Sep 7, 2023, 6:45 PM +00:00

--- test/test.py
+++ test/test.py
@@ -1,21 +1,6 @@
-from bs4 import BeautifulSoup
+import requests
 
-# Specify the path to your local HTML file
-file_path = 'C:\\Users\\sh\\Documents\\VeriFone\\SRNTemp\\radBA089.tmp_PLU.html'
-# OR
-file_path = r'C:\Users\sh\Documents\VeriFone\SRNTemp\radBA089.tmp_PLU.html'
+URL = "C:/Users/sh/Documents/VeriFone/SRNTemp/rad6BB33.tmp_PLU.html"
+page = requests.get(URL)
 
-
-# Open and read the HTML file
-with open(file_path, 'r', encoding='utf-8') as file:
-    html_content = file.read()
-
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(html_content, 'html.parser')
-
-# Now, you can extract data from the parsed HTML using BeautifulSoup methods
-# For example, to extract and print the title of the HTML page:
-title = soup.title.string
-print(f'Title: {title}')
-
-# You can also extract other elements, attributes, or text as needed
+print(page.text)
