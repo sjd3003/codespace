c3ff0488ac3588514acfec4400df8131d425bdf7
sjd3003 <143242493+sjd3003@users.noreply.github.com>
2023-09-07 18:22:12 +0000

    Thu, Sep 7, 2023, 6:22 PM +00:00

--- test/test.py
+++ test/test.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
 
 # Specify the path to your local HTML file
-file_path = 'C:/Users/sh/Documents/VeriFone/SRNTemp/radBA089.tmp_PLU.html'
+file_path = 'C:\\Users\\sh\\Documents\\VeriFone\\SRNTemp\\radBA089.tmp_PLU.html'
+# OR
+file_path = r'C:\Users\sh\Documents\VeriFone\SRNTemp\radBA089.tmp_PLU.html'
+
 
 # Open and read the HTML file
 with open(file_path, 'r', encoding='utf-8') as file:
