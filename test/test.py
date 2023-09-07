a26eae13e9dfa487a6efacb4dcebb34c148cff08
sjd3003 <143242493+sjd3003@users.noreply.github.com>
2023-09-07 18:12:02 +0000

    Thu, Sep 7, 2023, 6:12 PM +00:00

---
+++ test/test.py
@@ -0,0 +1,5 @@
+from bs4 import BeautifulSoup
+import requests
+url = 'file:///C:/Users/sh/Documents/VeriFone/SRNTemp/radBA089.tmp_PLU.html'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
